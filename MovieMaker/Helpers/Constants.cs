23ba82da3b78e16035c5865b015e5d3aea384e99
DanilLebets <stebel99@gmail.com>
2020-11-18 10:31:15 +0200

    Splited video and photo formats

--- MovieMaker/Helpers/Constants.cs
+++ MovieMaker/Helpers/Constants.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections.Generic;
-
-namespace MovieMaker.Helpers
+﻿namespace MovieMaker.Helpers
 {
     public static class Constants
     {
-        public static readonly string[] Formats =
+        public static readonly string[] VideoFormats =
         {
             ".webm",".mpg",".mp2",
             ".mpeg",".mpe",".mpv",
             ".ogg",".mp4",".m4p",
             ".m4v",".avi", ".wmv",
-            ".mov",".mkv",
+            ".mov",".mkv"
+        };
+
+        public static readonly string[] PhotoFormats =
+        {
             ".jpg", ".png", ".jpeg"
         };
 
